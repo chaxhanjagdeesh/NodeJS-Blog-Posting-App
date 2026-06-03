61b6421a4e0184da731a9ecc117b39ba3670d899
Jagdeesh Hari Chauhan <jagdeeshRupnagar@gmail.com>
2026-06-03 12:08:17 +0530

    Added Pagination

--- routes/main.js
+++ routes/main.js
@@ -4,8 +4,11 @@ const postModel = require("../models/post");
 const userModel = require("../models/user");
 
 router.get("/", async (req, res) => {
-    let posts = await postModel.find().populate('user');
-    res.render("main", { posts });
+    let page = parseInt(req.query.page) || 1;
+    let limit = 5;
+    let skip = (page - 1) * limit;
+    let posts = await postModel.find().populate('user').skip(skip).limit(limit);
+    res.render("main", { posts, page, limit });
 });
 
 
