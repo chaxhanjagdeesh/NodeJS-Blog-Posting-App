afd1b45141f93f49b48e771f4573c1f5d3a6b358
Jagdeesh Hari Chauhan <jagdeeshRupnagar@gmail.com>
2026-06-03 12:49:09 +0530

    Added Sort Selection

--- routes/main.js
+++ routes/main.js
@@ -5,10 +5,11 @@ const userModel = require("../models/user");
 
 router.get("/", async (req, res) => {
     let page = parseInt(req.query.page) || 1;
+    let sort = req.query.sort || "newest";
     let limit = 5;
     let skip = (page - 1) * limit;
-    let posts = await postModel.find().populate('user').skip(skip).limit(limit);
-    res.render("main", { posts, page, limit });
+    let posts = await postModel.find().populate('user').sort({date: sort === "newest" ? -1 : 1}).skip(skip).limit(limit);
+    res.render("main", { posts, page, limit, sort });
 });
 
 
